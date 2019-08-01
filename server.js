5446f4ed0b39246f1c0712fb183dd9c277185444
Sergii Pushkovskyi <Sergii.Pushkovskyi@infopulse.com>
2019-08-01 10:44:00 +0300

    Now data is read from file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const app = express();
-const data = require("./data/all-costs.json");
+// const data = require("./data/all-costs.json");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,10 +12,12 @@ app.get("/", (req, res) => {
   res.send("Root is opened");
 });
 
-app.get("/costs/", (req, res) => {
+app.get("/costs/", async (req, res) => {
+  const dataFromFile = await getData();
+
   let { category } = req.query;
   if (category) {
-    const filtered = data.products.filter(el =>
+    const filtered = dataFromFile.products.filter(el =>
       el.categories.includes(category)
     );
     const state = filtered.length > 0 ? "success" : "no products";
@@ -23,11 +27,12 @@ app.get("/costs/", (req, res) => {
     });
   }
 
-  res.send(data);
+  res.send(dataFromFile);
 });
 
-app.get("/costs/:id", (req, res) => {
-  const product = data.products.find(
+app.get("/costs/:id", async (req, res) => {
+  const dataFromFile = await getData();
+  const product = dataFromFile.products.find(
     p => parseInt(p.id) === parseInt(req.params.id)
   );
   const status = product ? "success" : "no such product";
@@ -41,6 +46,22 @@ app.get("/costs/:id", (req, res) => {
 app.post("/costs", (req, res) => {
   res.send(req.body);
 });
+
+async function getData() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      path.join(__dirname, "data", "all-costs.json"),
+      "utf-8",
+      (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(data));
+        }
+      }
+    );
+  });
+}
 
 app.use(function(req, res, next) {
   let err = new Error("not found");
