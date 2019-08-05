6f3ea894a88c4dda3f72e0d45459ad37d1dbeb81
Sergey Pushkovskiy <push_s@mail.ua>
2019-08-05 20:34:24 +0300

    Some small improvements. Ready to submit

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const notesController = require("./controllers/notesController");
-// const data = require("./data/all-costs.json");
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,38 +16,6 @@ app.get("/", (req, res) => {
 
 app.get("/costs", notesController.all);
 
-// app.get("/costs/", async (req, res) => {
-//   const allCosts = await notesController.all;
-//   console.log(allCosts);
-
-//   let { category } = req.query;
-//   if (category) {
-//     const filtered = dataFromFile.products.filter(el =>
-//       el.categories.includes(category)
-//     );
-//     const state = filtered.length > 0 ? "success" : "no products";
-//     return res.send({
-//       state,
-//       products: filtered
-//     });
-//   }
-
-//   res.json(allCosts);
-// });
-
-// app.get("/costs/:id", async (req, res) => {
-//   const dataFromFile = await getData();
-//   const product = dataFromFile.products.find(
-//     p => parseInt(p.id) === parseInt(req.params.id)
-//   );
-//   const status = product ? "success" : "no such product";
-
-//   res.send({
-//     status,
-//     products: product
-//   });
-// });
-
 app.get("/costs/:id", notesController.findById);
 
 app.post("/costs", notesController.insert);
@@ -56,22 +23,6 @@ app.post("/costs", notesController.insert);
 app.patch("/costs/:id", notesController.update);
 
 app.delete("/costs/:id", notesController.delete);
-
-async function getData() {
-  return new Promise((resolve, reject) => {
-    fs.readFile(
-      path.join(__dirname, "data", "all-costs.json"),
-      "utf-8",
-      (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(JSON.parse(data));
-        }
-      }
-    );
-  });
-}
 
 app.use(function(req, res, next) {
   let err = new Error("not found");
